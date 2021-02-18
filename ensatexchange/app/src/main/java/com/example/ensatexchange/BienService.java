70eee9e5251a019b307c915808c89cf298b8f168
firdaousms <firdaousmossaouir@gmail.com>
2021-02-18 22:50:58 +0100

    First & second interface

---
+++ ensatexchange/app/src/main/java/com/example/ensatexchange/BienService.java
@@ -0,0 +1,14 @@
+package com.example.ensatexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BienService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bien_service);
+    }
+}
