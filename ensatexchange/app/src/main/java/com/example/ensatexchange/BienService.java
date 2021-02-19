eeeb138593d42c77a59255492d5da27824e18b67
firdaousms <firdaousmossaouir@gmail.com>
2021-02-19 13:38:16 +0100

    Bien interface

--- ensatexchange/app/src/main/java/com/example/ensatexchange/BienService.java
+++ ensatexchange/app/src/main/java/com/example/ensatexchange/BienService.java
@@ -2,13 +2,26 @@ package com.example.ensatexchange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BienService extends AppCompatActivity {
 
+     public Button rechercherBien;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bien_service);
+        rechercherBien=findViewById(R.id.rechercher_btn);
+
+        rechercherBien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BienService.this,Bien.class);
+                startActivity(intent);
+            }
+        });
     }
 }
